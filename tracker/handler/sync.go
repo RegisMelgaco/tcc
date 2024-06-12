e86128ad6471fe4cdbfe419675921a37ca8786d8
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2024-06-12 15:32:38 -0300

    update tracker contract

--- tracker/handler/sync.go
+++ tracker/handler/sync.go
@@ -10,7 +10,12 @@ import (
 	"time"
 )
 
-type SyncInputOutput struct {
+type SyncOutput struct {
+	Self      NodeData   `json:"self"`
+	Neighbors []NodeData `json:"neighbors"`
+}
+
+type NodeData struct {
 	Email     string    `json:"email"`
 	PublicIP  string    `json:"public_ip"`
 	LocalIPs  []string  `json:"local_ips"`
@@ -22,7 +27,7 @@ func (h Handler) Sync(w http.ResponseWriter, r *http.Request) {
 
 	logger := logs.FromContext(r.Context(), operation).With(slog.Time("start", time.Now()))
 
-	var input SyncInputOutput
+	var input NodeData
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{"msg": "invalid json"})
@@ -49,15 +54,21 @@ func (h Handler) Sync(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var rs []SyncInputOutput
+	var output SyncOutput
+
+	output.Self = input
+
+	var rs []NodeData
 	for _, n := range newNodes {
-		rs = append(rs, SyncInputOutput{
+		rs = append(rs, NodeData{
 			Email:     n.Email,
 			PublicIP:  n.PublicIP,
 			LocalIPs:  n.LocalIPs,
 			UpdatedAt: n.UpdatedAt,
 		})
 	}
+
+	output.Neighbors = rs
 
 	if err = json.NewEncoder(w).Encode(rs); err != nil {
 		logger.Error("failed to write response", slog.String("err", err.Error()))
